cacc2d9e9bfd70ac50f4e9dc37eaf398969156fb
Raswan Sualdi <rasonesualdi@gmail.com>
2022-06-20 01:46:08 +0800

    user factory delete

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,16 +22,16 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        return [
-            'nama' => $this->faker->name(),
-            'email' => 'redaksijalurrempah@gmail.com',
-            'email_verified_at' => now(),
-            'telp' => '12323232',
-            'role' => 'admin',
-            'password' => bcrypt('bukabuka@123'), // password
-            'remember_token' => Str::random(10),
-            'domisili' => 1
-        ];
+        // return [
+        //     'nama' => $this->faker->name(),
+        //     'email' => 'redaksijalurrempah@gmail.com',
+        //     'email_verified_at' => now(),
+        //     'telp' => '12323232',
+        //     'role' => 'admin',
+        //     'password' => bcrypt('bukabuka@123'), // password
+        //     'remember_token' => Str::random(10),
+        //     'domisili' => 1
+        // ];
     }
 
     /**
