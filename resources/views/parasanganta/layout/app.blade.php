8c5480a8e55d5af004bd1f753d62f83864bd2e39
Ahmad Irfandi <ahmad7irfandi@gmail.com>
2022-06-19 18:02:41 +0800

    Feat: add google analytics script

--- resources/views/parasanganta/layout/app.blade.php
+++ resources/views/parasanganta/layout/app.blade.php
@@ -92,5 +92,17 @@
 
 
 </body>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-P32HEMNE79"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-P32HEMNE79');
+</script>
 
 </html>
