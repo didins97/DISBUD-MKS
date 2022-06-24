c4476c0bb66e1e931a6ad53fdb7783637c658050
Raswan Sualdi <rasonesualdi@gmail.com>
2022-06-25 00:36:54 +0800

    fix: wbtb looping thumbnail error

--- resources/views/labu/content/wbtb_detail.blade.php
+++ resources/views/labu/content/wbtb_detail.blade.php
@@ -37,7 +37,7 @@
                                 @foreach($konten_labu as $a)
                                 <div class="sidebar__post-single">
                                     <div class="sidebar__post-image">
-                                        <img src="{{ asset('storage/assets/foto/thumbnail/' . $detail->thumbnail) }}" alt="OPK"
+                                        <img src="{{ asset('storage/assets/foto/thumbnail/' . $a->thumbnail) }}" alt="OPK"
                                             class="radius-medium">
                                     </div><!-- /.sidebar__post-image -->
                                     <div class="sidebar__post-content">
